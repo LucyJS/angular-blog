f57ac1fcb8b9d47b72657bb82c927da469b507dc
LucyJS <lucyluney@gmail.com>
2023-10-19 07:34:52 -0300

    feat: correction of merges

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -11,5 +11,4 @@ export class MenuBarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -19,5 +19,4 @@ export class SmallCardComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
